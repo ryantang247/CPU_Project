08a7e11636bf18526f127c22a6c18a5299b2cb61
Ryan <ryantang_register@hotmail.com>
2023-04-28 20:22:56 +0800

    resolve conflict in previous file

---
+++ CPU.srcs/sources_1/new/data_mem.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.04.2023 20:02:55
+// Design Name: 
+// Module Name: data_mem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_mem(
+
+    );
+endmodule
